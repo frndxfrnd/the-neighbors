1f861c3edc3c24b521555a4f8f16660a2ba8ba7d
iiwii <22158218+iiwii@users.noreply.github.com>
2021-05-15 17:01:08 -0400

    post

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -1,8 +1,8 @@
 import source from 'bootstrap-icons/bootstrap-icons.svg'
 
-export default ({ name }) => {
+export default ({ name, className }) => {
   return (
-    <svg className='bi' width='1rem' height='1rem' fill='currentColor'>
+    <svg className={['bi', className].join(' ')} width='1rem' height='1rem' fill='currentColor'>
       <use xlinkHref={`${source}#${name}`} />
     </svg>
   )
